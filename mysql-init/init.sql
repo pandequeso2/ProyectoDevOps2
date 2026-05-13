53427e57f3c48325adf8ae2c7b2f1cfab35b35fa
pandequeso2 <ben.arayag@duocuc.cl>
2026-05-13 09:43:44 -0400

    FUNCIONA TOOOOODOOOOO SUUUU

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS despachosdb;
 CREATE DATABASE IF NOT EXISTS ventasdb;
+
+-- Esto es vital: otorga permisos al usuario sobre ambas DBs
 GRANT ALL PRIVILEGES ON despachosdb.* TO 'userdb'@'%';
 GRANT ALL PRIVILEGES ON ventasdb.* TO 'userdb'@'%';
 FLUSH PRIVILEGES;
